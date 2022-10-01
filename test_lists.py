cd6c33b26491c479c1474a500bab48084a47289e
Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-10-01 17:03:22 -0400

    edit first line

--- test_lists.py
+++ test_lists.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+myList = ["banana", "cherry", "apple"]
+
+print(myList)
+
+item = myList[-3]
+
+print(item)
+
+print()
+print()
+
+for item in myList:
+    print(item)
+
+
+if "cherry" in myList:
+    print("get it")
+else:
+    print("buggy")
