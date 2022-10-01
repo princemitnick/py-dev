21570f2da2bcb29ccaf767c91f6e6ea139767f0b
Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-10-01 18:15:44 -0400

    Edit test_lists.py

--- test_lists.py
+++ test_lists.py
@@ -66,3 +66,69 @@ print(reverseThirdNumberList)
 
 #Copy of list
 
+org_list = [1,2,3,4,5]
+
+cpy_list = org_list
+
+print(cpy_list)
+
+cpy_list.pop()
+
+print(org_list)
+
+
+#Test copy with an integer variable
+
+a = 4
+b = a
+print(a, b)
+
+b = 6
+
+print(a, b)
+
+
+#Test copy with a string variable
+
+a = "Mom"
+b = a
+
+print(a, b)
+
+b = "Dad"
+
+print(a, b)
+
+#Test copy with tuple
+
+t1 = (1, 2, 3, 4, 5, 6)
+
+t2 = t1
+
+print(t1)
+print(t2)
+
+t2 = (3,48,1,3)
+
+print(t1)
+print(t2)
+
+
+#Test copy of list with copy() method and list() "object) and slicing [:]
+
+myList = ["Jeremie", "Jacmel", "Cayes"]
+
+myList1 = list(myList)
+myList2 = myList1.copy()
+myList3 = myList2[:]
+
+print(myList)
+print(myList1)
+
+myList1.append("Gonaives")
+myList2.append("Saint-Marc")
+myList3.append("Mirebalais")
+print(myList)
+print(myList1)
+print(myList2)
+print(myList3)
